d1396283de11c2841c30cf412af0bc76bdbc4ce2
chicosilva1 <franciscoemannueldasilva@gmail.com>
2023-03-31 19:55:24 -0300

    Copia ai le

---
+++ Projeto WEB/Controllers/ProdutoController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Projeto_WEB.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        public IActionResult Lista()
+        {
+            return View();
+        }
+
+        public IActionResult Cadastrar() {
+            return View();
+        }
+    }
+}
